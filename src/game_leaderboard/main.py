390a1de098b42304f80d69d74aeddd092e7749f7
crosszay1 <Isaiahbfreundlich@Gmail.com>
2026-08-17 15:25:11 -0400

    fix import collision

--- src/game_leaderboard/main.py
+++ src/game_leaderboard/main.py
@@ -1,6 +1,6 @@
 import undetected_chromedriver as uc
 import logging
-from smails_box import SmailsMailbox #our mailbox thing
+from game_leaderboard.smails_box import SmailsMailbox #our mailbox thing
 from selenium.webdriver.common.by import By
 
 def main():
