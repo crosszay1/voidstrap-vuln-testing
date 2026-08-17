eb185db09e3092c96e2d0e4320e19fa8c8fda3f7
crosszay1 <Isaiahbfreundlich@Gmail.com>
2026-08-17 14:55:45 -0400

    Second commit

--- src/game_leaderboard/main.py
+++ src/game_leaderboard/main.py
@@ -1,10 +1,16 @@
 import undetected_chromedriver as uc
 
-driver = uc.Chrome()
 
-try:
-    driver.get("https://example.com")
-    print("Page title:", driver.title)
-    input("Press Enter to close...")
-finally:
-    driver.quit()
+def main():
+    driver = uc.Chrome()
+
+    try:
+        driver.get("https://example.com")
+        print("Page title:", driver.title)
+        input("Press Enter to close...")
+    finally:
+        driver.quit()
+
+
+if __name__ == "__main__":
+    main()
